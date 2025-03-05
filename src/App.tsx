5b9cd1d983618c5ad342ef6899e125fbd47cb1bc
DaveV6 <bujzasdavid@gmail.com>
2025-03-05 09:34:29 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <section className='h-screen bg-linear-to-r from-cyan-500 to-blue-500 flex items-center'>
+            <div className='container'>
+                <h1 className='text-center font-bold text-3xl'>
+                    Event Manager
+                </h1>
+            </div>
+        </section>
+    </>
+  )
+}
+
+export default App
