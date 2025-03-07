859107925dd742dc7415c16f297689a1248445d7
DaveV6 <bujzasdavid@gmail.com>
2025-03-07 17:42:55 +0100

    change date picker version

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import Table from './table/page';
-import { ThemeProvider } from "@/components/ThemeProvider";
+import Table from './table/page'
+import { ThemeProvider } from "@/components/ThemeProvider"
 
 function App() {
   return (
